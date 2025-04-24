e56328d8b41b8e542353c4831357f7e4a86fccbc
Andre-m3 <andrew.mnt02@gmail.com>
2025-04-24 17:19:02 +0200

    Update 2 - Minor Fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,25 @@
+import model.Amministratore;
+import model.UtenteGenerico;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        // Genero le istanze di due utenti a scopo di test. Cosi posso verificare senza difficoltà i vari metodi qui nel main
+        UtenteGenerico utente = new UtenteGenerico(
+                "giancarloMinecraft@gmail.com",
+                "gianmine10",
+                "Giancarlo10!"
+        );  // Utente -> Utente Generico
+
+        Amministratore amministratore = new Amministratore(
+                "andreamontella@capodichino.eu",
+                "admin-AndreaMontella",
+                "!MyAmazingPassword_3!"
+        );  // Utente -> Amministratore
+
+        amministratore.inserimentoVolo();           //Metodo accessibile dal solo Amministratore
+        utente.prenotaVolo();                       //Metodo accessibile dal solo Utente
     }
 }
